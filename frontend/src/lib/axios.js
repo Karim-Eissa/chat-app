15c5c5bcaa70036ed37e128b900dac5abe5bda27
Karim Eissa <karim.eissa@robustastudio.com>
2025-03-10 23:42:41 +0200

    deploying

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance=axios.create({
-    baseURL:"https://chatty-egypt.duckdns.org",
+    baseURL:"https://chatty-egypt.duckdns.org/api",
     withCredentials:true
 })
